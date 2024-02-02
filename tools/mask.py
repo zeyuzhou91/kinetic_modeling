3af3e9082b3b38dc7fc88ad1985d76ecce29bc88
Zeyu Zhou <zeyuzhou91@gmail.com>
2024-02-02 13:14:55 -0500

    added linear_transform in mask.py and tested it. updated variable names in mask.py

--- tools/mask.py
+++ tools/mask.py
@@ -23,19 +23,19 @@ def create_MR_mask_including(in_IDs, segfile_path, opROI_name, op_dir):
     ----------
     in_IDs : a list of integers
         The integer IDs of ROIs that the mask should include. 
-    segfile_path : string 
+    segfile_path : string, file path
         The path of the MR segmentation file from which the mask is created. 
         The file should be .nii or .nii.gz 
     opROI_name : string 
         The name of the output combined ROI. 
-    op_dir : string
+    op_dir : string, directory path
         The path of the output directory where the output mask is stored. 
 
     Returns
     -------
-    opfile_name: string
-        The name of the output mask file, ending in .nii.gz
-    opfile_path: string
+    opmask_fullname: string
+        The full name of the output mask file, ending in .nii.gz
+    opmask_path: string, file path
         The path of the output mask file. 
 
     """
@@ -52,13 +52,13 @@ def create_MR_mask_including(in_IDs, segfile_path, opROI_name, op_dir):
     # Make the nifti mask image
     mask_opROI = nib.Nifti1Image(mask_opROI_data, seg.affine, seg.header)
     
-    opfile_name = f'mask_mr_{opROI_name}.nii.gz'
-    opfile_path = os.path.join(op_dir, opfile_name)
+    opmask_fullname = f'mask_mr_{opROI_name}.nii.gz'
+    opmask_path = os.path.join(op_dir, opmask_fullname)
     
     # Save the nifti mask image
-    nib.save(mask_opROI, opfile_path)
+    nib.save(mask_opROI, opmask_path)
     
-    return opfile_name, opfile_path
+    return opmask_fullname, opmask_path
     
     
 
@@ -71,19 +71,19 @@ def create_MR_mask_excluding(ex_IDs, segfile_path, opROI_name, op_dir):
     ----------
     ex_IDs : a list of integers
         The integer IDs of ROIs that the mask should exclude. 
-    segfile_path : string 
+    segfile_path : string, file path 
         The path of the MR segmentation file from which the mask is created. 
         The file should be .nii or .nii.gz 
     opROI_name : string 
         The name of the output combined ROI. 
-    op_dir : string
+    op_dir : string, directory path
         The path of the output directory where the output mask is stored. 
 
     Returns
     -------
-    opfile_name: string
+    opmask_fullname: string
         The name of the output mask file, ending in .nii.gz
-    opfile_path: string
+    opmask_path: string, file path
         The path of the output mask file. 
 
     """
@@ -100,23 +100,91 @@ def create_MR_mask_excluding(ex_IDs, segfile_path, opROI_name, op_dir):
     # Make the nifti mask image
     mask_opROI = nib.Nifti1Image(mask_opROI_data, seg.affine, seg.header)
     
-    opfile_name = f'mask_mr_{opROI_name}.nii.gz'
-    opfile_path = os.path.join(op_dir, opfile_name)
+    opmask_fullname = f'mask_mr_{opROI_name}.nii.gz'
+    opmask_path = os.path.join(op_dir, opmask_fullname)
     
     # Save the nifti mask image
-    nib.save(mask_opROI, opfile_path)
+    nib.save(mask_opROI, opmask_path)
     
-    return opfile_name, opfile_path
+    return opmask_fullname, opmask_path
 
 
 
             
             
+def linear_transform(ipmask_path, ipmask_fullname, inDomain, outDomain, lta_path, thr, save_bfthr_mask, op_dir):
+    """
+    Performs linear transform of the input mask from inDomain to outDomain. 
 
-            
+    Parameters
+    ----------
+    ipmask_path : string, directory path
+        The path of the input mask .nii.gz file. 
+    iipmask_fullname : string
+        The full name of the input mask file, ending in .nii.gz. Should contain the inDomain string. 
+    inDomain : string
+        The input domain, 'mr' or 'pet'
+    outDomain : string
+        The output domain, 'mr' or 'pet'
+    lta_path : string, file path
+        The path of the .reg.lta file, containing information of the linear transform. 
+    thr : float in [0, 1]
+        The threshold for deciding 0 or 1 for decimal voxel values in output mask. 
+    save_bfthr_mask : boolean
+        True: save the intermediate decimal-valued mask before thresholding; False: do not save.  
+    op_dir : string, directory path
+        The path of the output directory where the output mask is stored. 
 
-            
-            
+    Returns
+    -------
+    opmask_fullname: string
+        The full name of the output mask file, ending in .nii.gz
+    opmask_path: string, file path
+        The path of the output mask file. 
+
+    """
+    
+    # dashed versions of inDomain and outDomain
+    inD = f'_{inDomain}_'
+    outD = f'_{outDomain}_'
+
+    assert inD in ipmask_fullname, f"The ipmask_fullname should contain {inD}"
+    
+    # split from the first occurrence of '.'
+    # e.g.: mask_mr_cerebellum.nii.gz => mask_mr_cerebellum and nii.gz
+    ipmask_basename, extension = ipmask_fullname.split('.', 1)
+    
+    # create the output mask'a base name by replacing the first occurence of inD in ipmask_name with outD
+    opmask_basename = ipmask_basename.replace(inD, outD, 1)
+    
+    # names and path of the bfthr output mask
+    opmask_bfthr_basename = opmask_basename + '_bfthr'
+    opmask_bfthr_fullname = opmask_bfthr_basename + '.' + extension
+    opmask_bfthr_path = os.path.join(op_dir, opmask_bfthr_fullname)
+    print(f'opmask_bfthr_path: {opmask_bfthr_path}')
+    
+    # map the input mask from inDomain to outDomain
+    # mapped mask has decimal values
+    os.system(f'$FREESURFER_HOME/bin/mri_convert -at {lta_path} {ipmask_path} {opmask_bfthr_path}')
+    
+    # load the bfthr output mask
+    opmask_bfthr = nib.load(opmask_bfthr_path)
+    opmask_bfthr_data = opmask_bfthr.get_fdata()
+    
+    # thresholding the bfthr output mask (decimal) to make it binary
+    opmask_data = (opmask_bfthr_data >= thr).astype(float)
+    opmask = nib.Nifti1Image(opmask_data, opmask_bfthr.affine, opmask_bfthr.header)
+    
+    # name and path of the binary output mask
+    opmask_fullname = opmask_basename + '.' + extension
+    opmask_path = os.path.join(op_dir, opmask_fullname)
+    nib.save(opmask, opmask_path)
+        
+    if save_bfthr_mask == False:
+        # delete the intermediate bfthr mask
+        os.remove(opmask_bfthr_path)
+
+    return opmask_fullname, opmask_path
             
             
 
